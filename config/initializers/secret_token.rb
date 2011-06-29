c960ceb2e51b29a3b35b732f64f27180a64b176e
Carlos León <mail@carlosleon.info>
2011-06-29 18:10:17 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devcolicious::Application.config.secret_token = '47e2c086f6db22d1e9a1812298f52192f4dd8cdc4791b46deecf5abd54e2c30380792db6e8748ea6ff888867e5fb632c44c6e2e3de1043b018aea3c92bace682'
