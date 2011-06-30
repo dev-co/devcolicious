563ad7e7f0d7be21e02e26fdc4d0669b47ae22d7
Carlos León <mail@carlosleon.info>
2011-06-29 19:02:32 -0500

    [Core] Updating application configuration, so tests can run properly.

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Devcolicious
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'America/Bogota'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -43,5 +43,11 @@ module Devcolicious
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    # Configure generators.
+    config.generators do |g|
+      g.test_framework      :rspec, :fixture => true
+      g.fixture_replacement :fabrication
+    end
   end
 end
