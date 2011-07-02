86f8208d6d61e9111fd92d7ad30d6c91bb8fcef4
Carlos León <mail@carlosleon.info>
2011-07-02 00:38:17 -0500

    [Core] Hidding flash message div after 3 seconds of being shown.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,13 @@
 // the compiled file.
 //
 //= require_tree .
+$( document ).ready( function(){
+  hide_flash_message();
+} );
+
+function hide_flash_message(){
+  var flash = $( '#flash' );
+  if ( undefined != flash ){
+    flash.delay( 3000 ).slideUp( 'slow' );
+  }
+}
