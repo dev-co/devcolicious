7d9bc220e3c0e0406f27a12c064c8e34438b567f
Carlos León <mail@carlosleon.info>
2011-07-02 00:30:43 -0500

    Fixing bad named div class at the application_helper view helper, and in the application.css file.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     result = ''
     if object.respond_to? :errors and object.errors.any?
       title  = "#{ pluralize( object.errors.count, 'error' ) } prohibited this #{ object.class.name.downcase } from being saved:"
-      result << '<div class="errors_explanation">\n'
+      result << '<div class="error_explanation">\n'
       result << "<h2>#{ title }</h2>\n"
       result << '<ul>\n'
       object.errors.full_messages.each do |message|
