fd6c76e272c5e9b3db854b9d2aa6d2b4f924cc18
Carlos León <mail@carlosleon.info>
2011-07-02 00:15:04 -0500

    [Core] Adding a view helper that lists the errors found for a model object.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def error_messages_for( object )
+    result = ''
+    if object.respond_to? :errors and object.errors.any?
+      title  = "#{ pluralize( object.errors.count, 'error' ) } prohibited this #{ object.class.name.downcase } from being saved:"
+      result << '<div class="errors_explanation">\n'
+      result << "<h2>#{ title }</h2>\n"
+      result << '<ul>\n'
+      object.errors.full_messages.each do |message|
+        result << "<li>#{ message }</li>\n"
+      end
+      result << '</ul>\n'
+      result << '</div>\n'
+    end
+    result.html_safe
+  end
+
 end
